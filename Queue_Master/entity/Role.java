1498c9bcd2d2250a860666a555f18dd2cb1c3f02
Abhi Wargad <wargadabhi@gmail.com>
2026-05-09 20:48:49 +0530

    initial commit

--- Queue_Master/entity/Role.java
+++
@@ -1,8 +0,0 @@
-package com.example.Queue_Master.entity;
-
-public enum Role {
-    USER,        // customer who books tokens
-    STAFF,       // doctor / counter staff
-    ADMIN,       // hospital manager / branch manager
-    SUPER_ADMIN  // platform-level administrator
-}
